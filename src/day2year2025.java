c875da2a887759d515e8ecb985ef83b3163b92dd
LeonWong <leonw24@nycstudents.net>
2025-12-08 09:52:28 -0500

    day3 completion part 1 part 2

--- src/day2year2025.java
+++ src/day2year2025.java
@@ -1,4 +1,59 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class day2year2025 {
+    public static void main(String[] args) {
+
+        // Day 2 data parsing set up
+        ArrayList<String> fileData = getFileData("src/data");
+        ArrayList<Long> productIDs = new ArrayList<Long>();
+        for (String s : fileData.get(0).split(",")) {
+            long first = Long.parseLong(s.split("-")[0]);
+            long second = Long.parseLong(s.split("-")[1]);
+            for (long i = first; i <= second; i++) {
+                productIDs.add(i);
+            }
+        }
+        // Day 2 data parsing set up complete
+        // "productIDs" is a list of "long" variables that make up each product ID from the file
+
+        long partOneAnswer = 0;
+
+        for (Long p : productIDs) {
+            if (isValidProductID(p)) {
+                partOneAnswer += p;
+            }
+
+        }
+
+        System.out.println("Part one answer: " + partOneAnswer);
+    }
+
+    public static boolean isValidProductID(long productID) {
+        String pID = productID + "";
+        String regex = "";
+        Matcher matches = Pattern.compile(regex).matcher(pID);
+        return matches.find();
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
 }
--- src/day3year2025.java
+++ src/day3year2025.java
@@ -1,4 +1,59 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class day3year2025 {
+    public static void main(String[] args) {
+
+        // **** DO NOT EDIT ANYTHING HERE ****
+        ArrayList<String> fileData = getFileData("src/data");
+        int partOneAnswer = 0;
+        for (String batteries : fileData) {
+            int voltage = getLargestCombination(batteries);
+            partOneAnswer += voltage;
+        }
+
+        System.out.println("Part one answer: " + partOneAnswer);
+    }
+
+    // COMPLETE THIS METHOD!
+    public static int getLargestCombination(String batteries) {
+        int firstNum = 0;
+        int secondNum = 0;
+        for (int i = 9; ; i--) {
+            if (batteries.substring(0, batteries.length() - 1).contains(i + "")) {
+                firstNum = i;
+                break;
+            }
+        }
+        String cutStr = batteries.substring(batteries.indexOf(firstNum + "") + 1);
+        for (int i = 9; i > 0; i--) {
+            if (cutStr.contains(i + "")) {
+                secondNum = i;
+                break;
+            }
+        }
+        System.out.println(firstNum + "" + secondNum);
+        System.out.println(cutStr);
+        return firstNum * 10 + secondNum;
+    }
+
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
 }
