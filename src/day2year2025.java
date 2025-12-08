274b9a54b35368db41373fcc849fdfe1299fd0b2
s <@bths.edu>
2025-12-08 12:29:09 -0500

    day 2 part 2 completed

--- src/day2year2025.java
+++ src/day2year2025.java
@@ -2,8 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class day2year2025 {
     public static void main(String[] args) {
@@ -22,6 +20,7 @@ public class day2year2025 {
         // "productIDs" is a list of "long" variables that make up each product ID from the file
 
         long partOneAnswer = 0;
+        long partTwoAnswer = 0;
 
         for (Long p : productIDs) {
             if (isValidProductID(p)) {
@@ -29,15 +28,39 @@ public class day2year2025 {
             }
 
         }
+        for (Long p : productIDs) {
+            if (isValidProductID2(p)) {
+                partTwoAnswer += p;
+            }
+        }
 
         System.out.println("Part one answer: " + partOneAnswer);
+        System.out.println("Part two answer: " + partTwoAnswer);
     }
 
     public static boolean isValidProductID(long productID) {
         String pID = productID + "";
-        String firstHalf = pID.substring(0, pID.length()/2);
-        String secondHalf = pID.substring(pID.length()/2);
+        String firstHalf = pID.substring(0, pID.length() / 2);
+        String secondHalf = pID.substring(pID.length() / 2);
         return secondHalf.equals(firstHalf);
+    }
+
+    public static boolean isValidProductID2(long productID) {
+        String pID = productID + "";
+
+        for (int i = 1; i <= pID.length() / 2; i++) {
+            String cutStr = pID.substring(0, i);
+            boolean isValid = true;
+            for (int j = i; j < pID.length(); j += i) {
+                if (pID.length() % i != 0) {
+                    isValid = false;
+                    break;
+                }
+                if (!cutStr.equals(pID.substring(j, j + i))) isValid = false;
+            }
+            if (isValid) return true;
+        }
+        return false;
     }
 
     public static ArrayList<String> getFileData(String fileName) {
