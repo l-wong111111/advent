fbb49a35454051dbbfa87de417386eb76106f8e0
s <@bths.edu>
2025-12-12 11:51:36 -0500

    day 7 part 2 complete

--- src/day7year2025.java
+++ src/day7year2025.java
@@ -7,6 +7,11 @@ public class day7year2025 {
     public static void main(String[] args) {
         ArrayList<String> fileData = getFileData("src/data");
         String[][] grid = get2DArray(fileData);
+        System.out.println(part1(grid));
+        System.out.println((long) part2(grid));
+    }
+
+    public static int part1(String[][] grid) {
         // set position to first line
         // change data file first line from S to |
         String position = ".......................................................................|.......................................................................";
@@ -25,7 +30,33 @@ public class day7year2025 {
             System.out.println(inc);
             System.out.println(position);
         }
-        System.out.println(count);
+        return (count);
+    }
+
+    public static double part2(String[][] grid) {
+        // change data file first line from S to |
+        // part 2 easier than part 1 imo since coding part 1 was harder
+        ArrayList<Double> position = new ArrayList<>();
+        for (int i = 0; i < grid.length; i++) {
+            position.add(0.0);
+        }
+        position.set(position.size() / 2 - 1, 1.0);
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                System.out.print(grid[i][j]);
+                if (grid[i][j].equals("^")) {
+                    position.set(j - 1, position.get(j - 1) + position.get(j));
+                    position.set(j + 1, position.get(j + 1) + position.get(j));
+                    position.set(j, 0.0);
+                }
+            }
+            System.out.println("       " + position);
+        }
+        double finalVal = 0;
+        for (int i = 0; i < position.size(); i++) {
+            finalVal += position.get(i);
+        }
+        return (finalVal);
     }
 
     public static String[][] get2DArray(ArrayList<String> fileData) {
