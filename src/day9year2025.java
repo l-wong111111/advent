0a92473f11c018f758f8433d878c3b94a681cda5
LeonWong <leonw24@nycstudents.net>
2025-12-16 10:17:40 -0500

    day 9 part 1

---
+++ src/day9year2025.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class day9year2025 {
+    public static void main(String[] args) {
+        ArrayList<String> fileData = getFileData("src/data");
+        ArrayList<Long> nums1 = new ArrayList<>();
+        ArrayList<Long> nums2 = new ArrayList<>();
+        ArrayList<Long> areas = new ArrayList<>();
+        for (int i = 0; i < fileData.size(); i++) {
+            nums1.add(Long.parseLong(fileData.get(i).split(",")[0]));
+            nums2.add(Long.parseLong(fileData.get(i).split(",")[1]));
+        }
+        System.out.println(nums2);
+        System.out.println(nums1);
+        for (int i = 0; i < nums1.size(); i++) {
+            for (int j = 0; j < nums2.size(); j++) {
+                areas.add(((Math.abs(nums1.get(i) - nums1.get(j)) + 1) * (Math.abs(nums2.get(i) - nums2.get(j)) + 1))); // use + 1 because finding the difference in length and width and adding to the missing initial length and width
+            }
+        }
+        Long largestVal = Collections.max(areas);
+        System.out.println(largestVal);
+
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
+}
