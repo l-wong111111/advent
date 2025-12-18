ecee1d66008a0861da1dc79317d4bb3d36beb1b4
s <@bths.edu>
2025-12-18 12:13:28 -0500

    final CustomAdventFileGenerator fix

--- src/CustomAdventFileGenerator.java
+++ src/CustomAdventFileGenerator.java
@@ -1,18 +1,23 @@
 public class CustomAdventFileGenerator {
     public static void main(String[] args) {
-        int size = (int) (Math.random() * 100) * 2 + 501;
-        //int size = 125;
+        int size = (int) (Math.random() * 100) * 2 + 301; //the console cannot handle rows or columns over a value near 500 so the range has to be 301-501 here
+        // 301 can be replaced with another value if console can handle it
+
+        //int size = 605; // this won't work with intelliJ
         String str = "";
+        int ring = 0;
         int num = (size / 2) + 1;
         for (int i = 1; i <= size; i++) {
-            int ring = 0;
             for (int j = 1; j <= size; j++) {
                 int valI = num - Math.abs(num - i);
                 int valJ = num - Math.abs(num - j);
                 ring = Math.min(valI, valJ);
                 if (j != size) System.out.print(ring - (int) (Math.sqrt((Math.pow(Math.random(), 5) * ring + 0.9) * ring)) + ".");
+
+                //bug testing code below
                 //if (ring - (int) (Math.sqrt((Math.pow(Math.random(), 8) * ring + 0.9) * ring)) > ring)
-                    //System.out.println("error");  //code used to test because it somehow generated numbers greater than the ring number
+                    //System.out.println("error");
+                //System.out.print(valI + ".");
             }
             System.out.println(ring - (int) (Math.sqrt((Math.pow(Math.random(), 5) * ring + 0.9) * ring)));
         }
