531c8f2d4413a2bea32bb3cdc075faac5f80c31b
LeonWong <leonw24@nycstudents.net>
2025-12-18 10:05:55 -0500

    custom advent question data file generator complete

--- src/CustomAdventFileGenerator.java
+++ src/CustomAdventFileGenerator.java
@@ -1,18 +1,19 @@
 public class CustomAdventFileGenerator {
     public static void main(String[] args) {
-        //int size = (int) (Math.random() * 100) * 2 + 501;
-        int size = 5;
+        int size = (int) (Math.random() * 100) * 2 + 501;
+        //int size = 25;
         String str = "";
         int num = (size / 2) + 1;
         for (int i = 1; i <= size; i++) {
+            int ring = 0;
             for (int j = 1; j <= size; j++) {
-                int maxValI = num - Math.abs(num - i);
-                int maxValJ = num - Math.abs(num - j);
-                int smallerVal = maxValJ;
-                if (maxValI < maxValJ) smallerVal = maxValI;
-                System.out.print((int) (Math.random() * smallerVal) + ".");
+                int valI = num - Math.abs(num - i);
+                int valJ = num - Math.abs(num - j);
+                ring = valJ;
+                if (valI < valJ) ring = valI;
+                if (j != size) System.out.print(ring - (int) (Math.sqrt((Math.pow(Math.random(), 8) * ring + 0.9) * ring)) + ".");
             }
-            System.out.println();
+            System.out.println(ring - (int) (Math.sqrt((Math.pow(Math.random(), 8) * ring + 0.9) * ring)));
         }
     }
 }
