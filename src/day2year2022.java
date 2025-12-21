9786ff57ee41f67431cde883e7bd9869535b029d
leon wong <leonw24@nycstudents.net>
2025-12-21 13:48:33 -0500

    day 2 year 2022 part 1 complete

--- src/day2year2022.java
+++ src/day2year2022.java
@@ -8,26 +8,36 @@ public class day2year2022 {
         ArrayList<String> fileData = getFileData("src/data");
         ArrayList<String> opp = new ArrayList<>();
         ArrayList<String> you = new ArrayList<>();
-        int totalScore = 0;
+        int score = 0;
         for (int i = 0; i < fileData.size(); i++) {
-            opp.add(fileData.get(i).split(" ")[0]);
+            opp.add(fileData.get(i).split(" ")[0    ]);
             you.add(fileData.get(i).split(" ")[1]);
         }
+        System.out.println(opp);
+        System.out.println(you);
         for (int i = 0; i < you.size(); i++) {
             String conversion = "";
-            String yourVal = opp.get(i);
-            int score = 0;
-            if (yourVal.equals("A")) conversion = "X";
-            if (yourVal.equals("B")) conversion = "Y";
-            if (yourVal.equals("C")) conversion = "Z";
+            String oppVal = opp.get(i);
+            if (oppVal.equals("A")) conversion = "X";
+            if (oppVal.equals("B")) conversion = "Y";
+            if (oppVal.equals("C")) conversion = "Z";
             if (conversion.equals(you.get(i))) {
                 score += 3;
             }
-            if (yourVal.equals("X")) {
+            if (you.get(i).equals("X")) {
                 score++;
-                if (yourVal.equals("Y")) score
+                if (conversion.equals("Z")) score += 6;
+            }
+            if (you.get(i).equals("Y")) {
+                score += 2;
+                if (conversion.equals("X")) score += 6;
+            }
+            if (you.get(i).equals("Z")) {
+                score += 3;
+                if (conversion.equals("Y")) score += 6;
             }
         }
+        System.out.println(score);
     }
 
     public static ArrayList<String> getFileData(String fileName) {
