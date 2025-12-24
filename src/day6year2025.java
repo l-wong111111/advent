742b8b86fdfe0dedbacd2465dbed54d5bd8b6a1c
leon wong <leonw24@nycstudents.net>
2025-12-24 01:14:02 -0500

    day 6 part 1 complete

--- src/day6year2025.java
+++ src/day6year2025.java
@@ -7,14 +7,40 @@ import java.util.Arrays;
 public class day6year2025 {
     public static void main(String[] args) {
         ArrayList<String> fileData = getFileData("src/data");
-        Integer[][] numbers = new Integer[3][1000000];
-        for (int i = 0; i < 3; i++) {
-            String[] line = fileData.get(i).split(" ");
-            for (int j = 0; j < line.length ; j++) {
-                numbers[i][j] = Integer.parseInt(line[j]);
+        ArrayList<String> signs = new ArrayList<>();
+        Integer[][] numbers = new Integer[4][1000000];
+        long totalVal = 0;
+        System.out.println(fileData);
+        for (int i = 0; i < 4; i++) {
+            String line = fileData.get(i);
+            for (int j = 0; j < line.split("\\s+").length ; j++) {
+                numbers[i][j] = Integer.parseInt(line.split("\\s+")[j]);
+                System.out.print(numbers[i][j] + " ");
             }
+            System.out.println();
         }
-        System.out.println(numbers);
+        String line = fileData.get(4);
+        for (int j = 0; j < line.split("\\s+").length ; j++) {
+            signs.add(line.split("\\s+")[j]);
+        }
+        System.out.println(signs);
+        for (int i = 0; i < line.split("\\s+").length; i++) {
+            Long val = 0L;
+            if (signs.get(i).equals("+")) {
+                val += numbers[0][i];
+                val += numbers[1][i];
+                val += numbers[2][i];
+                val += numbers[3][i];
+            } else {
+                val += numbers[0][i];
+                val *= numbers[1][i];
+                val *= numbers[2][i];
+                val *= numbers[3][i];
+            }
+            if (val < 0) System.out.println(); //checks if any integers are more than 32 bit int limit which means have to change data types to long
+            totalVal += val;
+        }
+        System.out.println(totalVal);
     }
 
     public static ArrayList<String> getFileData(String fileName) {
