4d20050238199f34fd208c302f2b5a5e781ce978
leon wong <leonw24@nycstudents.net>
2026-01-05 19:31:35 -0500

    day 9 part 2 progress (i hope this strategy works)

--- src/day9year2025.java
+++ src/day9year2025.java
@@ -32,22 +32,55 @@ public class day9year2025 {
     }
 
     public static long part2() {
+        // planning notes here: first find every connection between the points by finding
+        // coords with common x values or common y values
+
+        //  ..............  lines can make these shape things possibly so make a maze like algorithm
+        //  #XXXXXX#XXX#..  where you find spots that wont be part of a shape and see if
+        //  X......XXXXX..  rectangles contain them to decide if you ignore the rectangle area or not
+        //  X.#XXXX#XXXX..
+        //  X.XXXXXXXXXX..
+        //  X.#XXXXXX#XX..
+        //  X........XXX..
+        //  X........#X#..
+        //  #.............
         ArrayList<String> fileData = getFileData("src/data");
         ArrayList<Long> nums1 = new ArrayList<>();
         ArrayList<Long> nums2 = new ArrayList<>();
+        ArrayList<String> connections = new ArrayList<>();
+        ArrayList<String> connectionPoints = new ArrayList<>();
+        String[][] map = new String[100][100];
         ArrayList<Long> areas = new ArrayList<>();
+
         for (int i = 0; i < fileData.size(); i++) {
             nums1.add(Long.parseLong(fileData.get(i).split(",")[0]));
             nums2.add(Long.parseLong(fileData.get(i).split(",")[1]));
         }
+
         for (int i = 0; i < nums1.size(); i++) {
-            for (int j = 0; j < nums2.size(); j++) {
-                areas.add(((Math.abs(nums1.get(i) - nums1.get(j)) + 1) * (Math.abs(nums2.get(i) - nums2.get(j)) + 1)));
-                if (nums1.get(i).equals(nums1.get(j)) || nums2.get(i).equals(nums2.get(j))) System.out.println(nums1.get(i) + " " + nums2.get(i) + "   " + nums1.get(j) + " " + nums2.get(j));
+            for (int j = i + 1; j < nums1.size(); j++) {
+                if (nums1.get(i) == nums1.get(j)) {
+                    connections.add(fileData.get(i) + " " + fileData.get(j));
+                    connectionPoints.add(fileData.get(i));
+                    connectionPoints.add(fileData.get(j));
+                }
+                if (nums2.get(i) == nums2.get(j)) {
+                    connections.add(fileData.get(i) + " " + fileData.get(j));
+                    connectionPoints.add(fileData.get(i));
+                    connectionPoints.add(fileData.get(j));
+                }
             }
         }
-        Long largestVal = Collections.max(areas);
-        return largestVal;
+        System.out.println(connections);
+        for (int i = 0; i < connectionPoints.size(); i++) {
+            int xVal = Integer.parseInt(connectionPoints.get(i).split(",")[0]);
+            int yVal = Integer.parseInt(connectionPoints.get(i).split(",")[1]);
+            map[xVal][yVal] = "X";
+            for (int j = ; j < ; j++) {
+
+            }
+        }
+        return 2;
     }
 
     public static ArrayList<String> getFileData(String fileName) {
