22ed37a38411bf23ba56e2ee02cbc70ffce0cb4d
leon wong <leonw24@nycstudents.net>
2026-04-25 00:51:58 -0400

    day 4 year 2024 part 2 complete

--- src/day4year2024.java
+++ src/day4year2024.java
@@ -39,16 +39,19 @@ public class day4year2024 {
     public static int part2() {
         int ans = 0;
         ArrayList<String> fileData = getFileData("src/data");
+        ArrayList<String> coords = new ArrayList<>();
         for (int i = 0; i < fileData.size(); i++) {
             for (int j = 0; j < fileData.get(i).length(); j++) {
+                int temp = 0;
                 if (i < fileData.size() - 2 && j < fileData.get(i).length() - 2) { //diagonal up to down
-                    if (fileData.get(i).charAt(j) == 'X' && fileData.get(i + 1).charAt(j + 1) == 'M' && fileData.get(i + 2).charAt(j + 2) == 'A' && fileData.get(i + 3).charAt(j + 3) == 'S') ans++;
-                    if (fileData.get(i).charAt(j) == 'S' && fileData.get(i + 1).charAt(j + 1) == 'A' && fileData.get(i + 2).charAt(j + 2) == 'M' && fileData.get(i + 3).charAt(j + 3) == 'X') ans++;
+                    if (fileData.get(i).charAt(j) == 'M' && fileData.get(i + 1).charAt(j + 1) == 'A' && fileData.get(i + 2).charAt(j + 2) == 'S') temp++;
+                    if (fileData.get(i).charAt(j) == 'S' && fileData.get(i + 1).charAt(j + 1) == 'A' && fileData.get(i + 2).charAt(j + 2) == 'M') temp++;
                 }
-                if (i >= 2 && j < fileData.get(i).length() - 2) { //diagonal down to up
-                    if (fileData.get(i).charAt(j) == 'X' && fileData.get(i - 1).charAt(j + 1) == 'M' && fileData.get(i - 2).charAt(j + 2) == 'A' && fileData.get(i - 3).charAt(j + 3) == 'S') ans++;
-                    if (fileData.get(i).charAt(j) == 'S' && fileData.get(i - 1).charAt(j + 1) == 'A' && fileData.get(i - 2).charAt(j + 2) == 'M' && fileData.get(i - 3).charAt(j + 3) == 'X') ans++;
+                if (i < fileData.size() - 2 && j < fileData.get(i).length() - 2) { //diagonal down to up
+                    if (fileData.get(i + 2).charAt(j) == 'M' && fileData.get(i + 1).charAt(j + 1) == 'A' && fileData.get(i).charAt(j + 2) == 'S') temp++;
+                    if (fileData.get(i + 2).charAt(j) == 'S' && fileData.get(i + 1).charAt(j + 1) == 'A' && fileData.get(i).charAt(j + 2) == 'M') temp++;
                 }
+                if (temp == 2) ans++;
             }
         }
         return ans;
