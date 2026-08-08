9b2369c3add35dea576dbfd6664680b524e7cc07
leon wong <leonw24@nycstudents.net>
2026-08-08 10:07:28 +0800

    day 7 year 2024 part 2 progress, code works for sample data but not actual data

--- src/day7year2024.java
+++ src/day7year2024.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.rmi.MarshalException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -43,6 +44,38 @@ public class day7year2024 {
     public static int part2() {
         int ans = 0;
         ArrayList<String> fileData = getFileData("src/data");
+        for (int i = 0; i < fileData.size(); i++) {
+            long val = Long.parseLong(fileData.get(i).split(":")[0]);
+            String[] tempNums = fileData.get(i).split(" ");
+            int[] nums = new int[tempNums.length - 1];
+            for (int j = 0; j < nums.length; j++) {
+                nums[j] = Integer.parseInt(tempNums[j + 1]);
+            }
+
+            for (int j = 0; j < Math.pow(3, nums.length); j++) {
+                long result = nums[0];
+                int num = j;
+                for (int k = 1; k < nums.length; k++) {
+                    int option = num % 3;
+                    if (option == 0) {
+                        result += nums[k];
+                    }
+                    if (option == 1) {
+                        result *= nums[k];
+                    }
+                    if (option == 2) {
+                        String combined = result + "" + nums[k];
+                        result = Long.parseLong(combined);
+                    }
+                    num /= 3;
+                }
+                if (result == val) {
+                    ans += val;
+                    System.out.println(Arrays.toString(nums));
+                    break;
+                }
+            }
+        }
         return ans;
     }
 
